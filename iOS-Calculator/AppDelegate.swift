ba4a63b01a6968127ab55636edfbf56ce0fcfcb5
Adrian Estevez Rodriguez <adrestrod123@gmail.com>
2020-11-16 19:16:22 +0100

    Initial Commit

---
+++ iOS-Calculator/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Adrian Estevez 
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        // Setup
+        setupView()
+        
+        return true
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupView() {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+
+}
+
